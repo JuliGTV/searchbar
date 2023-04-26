59ca374fffab72bc44e737911520121754937775
Julius Vidal <jgtvidal@gmail.com>
2023-04-26 14:55:44 +0100

    created endpoint from which to get suggestions

--- backend/searchbar/first.py
+++ backend/searchbar/first.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
+suggestions = [
+    {'deletable': True, 'content': "https://imgur.com/a/O25jaEb", 'description': "Zebedee"},
+    {"deletable": True, "content": "http://www.google.com/search?q=${text}", "description": "Search Google for: ${text}"},
+    {"deletable": True, "content": "https://translate.google.co.uk/?sl=auto&tl=en&text=${text}%0A&op=translate", "description" :"translate ${text} to English"}
+    ]
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route('/suggestions')
+def get_suggestions():
+    return jsonify(suggestions)
