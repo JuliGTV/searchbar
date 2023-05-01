e1754027152b1b5486db0a8b814a59162df50332
Julius Vidal <jgtvidal@gmail.com>
2023-05-01 23:51:17 +0100

    works except for translate

--- chrome/background.js
+++ chrome/background.js
@@ -1,8 +1,3 @@
-function makeSuggestion(text){
-    console.log('make sug')
-    return {'content': 'https://www.atlassian.com/git/tutorials/using-branches?section=git-branch', 'description': `send [${text}] to the backend`}
-    
-}
 
 chrome.omnibox.onInputStarted.addListener(function() {
     chrome.omnibox.setDefaultSuggestion({description: "Select one of the options below."})
@@ -13,11 +8,11 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
     fetch("http://127.0.0.1:5000/suggestions/q=" + encodeURIComponent(text))
     .then(res => res.text())
     .then(data => {
-    suggest([{'content': data, 'description': 'go to: ' + data}])
+    console.log(data)
+    suggest([{'content': data.slice(1,-2), 'description': 'go to: ' + data.slice(1,-2)}])   //weird slicing necessary to get valid url for some reason (remove "")
 })
 })
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
-    console.log(text)
     chrome.tabs.update({'url': text})
 })
