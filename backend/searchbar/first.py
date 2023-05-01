969eb7269f7f6f0443aa9faaaebb1e0af719d9aa
Julius Vidal <jgtvidal@gmail.com>
2023-05-01 15:54:01 +0100

    backend now handles input

--- backend/searchbar/first.py
+++ backend/searchbar/first.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from urllib.parse import unquote
 
 app = Flask(__name__)
 CORS(app)
@@ -10,8 +11,11 @@ suggestions = [
     {"deletable": True, "content": "https://translate.google.co.uk/?sl=auto&tl=en&text=${text}%0A&op=translate", "description" :"translate ${text} to English"}
     ]
 
+def search(input):
+    print(input)
+    return {'deletable': True, 'content': "https://imgur.com/a/O25jaEb", 'description': "Zebedee"}
 
-@app.route('/suggestions')
-def get_suggestions():
-    return jsonify(suggestions)
+@app.route('/suggestions/q=<input>')
+def get_suggestions(input):
+    return jsonify(search(unquote(input)))
     # return("hello world")
