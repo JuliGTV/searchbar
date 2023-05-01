d3b20da36bf11030c6fb2759163600d1088ea336
Julius Vidal <jgtvidal@gmail.com>
2023-05-01 22:17:59 +0100

    name change to main

--- backend/bootstrap.cmd
+++ backend/bootstrap.cmd
@@ -1,2 +1,2 @@
-set FLASK_APP=searchbar\first
+set FLASK_APP=searchbar\main
 pipenv run flask --debug run -h 0.0.0.0
--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export FLASK_APP=./searchbar/first.py
+export FLASK_APP=./searchbar/main.py
 pipenv run flask --debug run -h 0.0.0.0
