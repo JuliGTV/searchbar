f60818a49032ed7aee05f6695e43c526ef433aad
Julius Vidal <jgtvidal@gmail.com>
2023-05-01 22:19:33 +0100

    uses proper backend

--- chrome/background.js
+++ chrome/background.js
@@ -1,5 +1,7 @@
 function makeSuggestion(text){
-    return {'content': text, 'description': `send [${text}] to the backend`}
+    console.log('make sug')
+    return {'content': 'https://www.atlassian.com/git/tutorials/using-branches?section=git-branch', 'description': `send [${text}] to the backend`}
+    
 }
 
 chrome.omnibox.onInputStarted.addListener(function() {
@@ -8,13 +10,14 @@ chrome.omnibox.onInputStarted.addListener(function() {
 
 
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-    suggest(makeSuggestion(text))
-})
-
-chrome.omnibox.onInputEntered.addListener(function(text) {
     fetch("http://127.0.0.1:5000/suggestions/q=" + encodeURIComponent(text))
     .then(res => res.text())
     .then(data => {
-    chrome.tabs.update({url: data});
+    suggest([{'content': data, 'description': 'go to: ' + data}])
 })
+})
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+    console.log(text)
+    chrome.tabs.update({'url': text})
 })
