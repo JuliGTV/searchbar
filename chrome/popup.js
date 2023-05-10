52118ee73f9ebbfd873438642b0df1598aaf5d49
Julius Vidal <jgtvidal@gmail.com>
2023-05-10 18:34:29 +0100

    popup

---
+++ chrome/popup.js
@@ -0,0 +1,13 @@
+//document.addEventListener('DOMContentLoaded', documentEvents  , false);
+
+function submit(input1, input2) { 
+    console.log(input1.value + "   " + input2.value);
+    chrome.runtime.sendMessage(message = {input1 : input2})
+    // console.log("Triggered!!!")
+}
+console.log("something")
+   
+document.getElementById('form').addEventListener('submit', 
+  function() { submit(document.getElementById('key'), document.getElementById('url'));
+});
+
