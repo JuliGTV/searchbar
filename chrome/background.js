e2688a2c788221d498d3d248bcbd1e12bc5df2ad
Julius Vidal <jgtvidal@gmail.com>
2023-05-10 10:43:30 +0100

    comment

--- chrome/background.js
+++ chrome/background.js
@@ -6,7 +6,7 @@ chrome.omnibox.onInputStarted.addListener(function() {
 var text2
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {            
     text2 = text
-    suggest([{'content': "https://www.google.com/search?q=" + text, 'description': "input "+ text}])   //weird slicing necessary to get valid url for some reason (remove "")
+    suggest([{'content': "https://www.google.com/search?q=" + text, 'description': "input "+ text}])   
 })
 
 
@@ -15,6 +15,6 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
     .then(res => res.text())
     .then(data => {
     console.log(data)
-    chrome.tabs.update({'url': data.slice(1,-2)})
+    chrome.tabs.update({'url': data.slice(1,-2)})       //weird slicing necessary to get valid url for some reason (remove "")
 })
 }) 
