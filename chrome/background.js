2af91b84282e6affc5950cf09904a4969dbf4aa9
Julius Vidal <jgtvidal@gmail.com>
2023-05-12 12:58:18 +0100

    popup now checks inputs are valid

--- chrome/background.js
+++ chrome/background.js
@@ -11,19 +11,31 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
 
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
-    fetch("http://127.0.0.1:8080/suggestions/q=" + encodeURIComponent(text2))
-    .then(res => res.text())
-    .then(data => {
-    console.log(data)
-    chrome.tabs.update({'url': data.slice(1,-2)})       //weird slicing necessary to get valid url for some reason (remove "")
+    chrome.identity.getProfileUserInfo()
+    .then(res => res.id)
+    .then(id => {
+        fetch(`http://127.0.0.1:8080/suggestions/${id}/q=${encodeURIComponent(text2)}`)
+        .then(res => res.text())
+        .then(data => {
+        console.log(data)
+        chrome.tabs.update({'url': data.slice(1,-2)})       //weird slicing necessary to get valid url for some reason (remove "")
+        })
+    }) 
 })
-}) 
+
 
 chrome.runtime.onMessage.addListener(function(message){
     console.log("message recieved")
     console.log(JSON.stringify(message))
-    fetch("http://127.0.0.1:8080/input-suggestion", {                                 //"https://searchbar-iaklo4m3da-uc.a.run.app/input-suggestion",{
-        method: "POST",
-        body: JSON.stringify(message)
+    
+    chrome.identity.getProfileUserInfo()
+    .then(res => res.id)
+    .then(id => {
+        console.log(id)
+    
+        fetch("http://127.0.0.1:8080/input-suggestion", {                                 //"https://searchbar-iaklo4m3da-uc.a.run.app/input-suggestion",{
+            method: "POST",
+            body: JSON.stringify({'id': id, 'new':message})
+        })
     })
 }) 
--- chrome/popup.js
+++ chrome/popup.js
@@ -5,9 +5,31 @@ function submit(input1, input2) {
     chrome.runtime.sendMessage(message = {[input1.value] : input2.value})
     // console.log("Triggered!!!")
 }
-console.log("something")
+function isValidUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return true;
+}
    
 document.getElementById('form').addEventListener('submit', 
-  function() { submit(document.getElementById('key'), document.getElementById('url'));
+  function() { 
+    var key = document.getElementById('key')
+    var url = document.getElementById('url')
+
+    if (key == "" || " " in key){
+      alert("Please input a keyword with no spaces.")
+    } else if (!isValidUrl(url)) {
+      alert("Please input a valid URL.")
+    } else{
+      submit(key, url);
+    }
+    
+    
 });
 
