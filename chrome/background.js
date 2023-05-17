04792f925fd3e62116e5ae0c7142f9c155fca278
Julius Vidal <jgtvidal@gmail.com>
2023-05-17 18:23:06 +0100

    improved logging

--- chrome/background.js
+++ chrome/background.js
@@ -3,15 +3,16 @@
 //     chrome.omnibox.setDefaultSuggestion({description: "Click below"})
 // })
 
-
+var domain = "http://127.0.0.1:8080/"
 chrome.omnibox.onInputEntered.addListener(function(text) {
     chrome.identity.getProfileUserInfo()
     .then(res => res.id)
     .then(id => {
-        fetch(`http://127.0.0.1:8080/suggestions/${id}/q=${encodeURIComponent(text)}`)
+        console.log("getting url for: " + text)
+        fetch(domain + `suggestions/${id}/q=${encodeURIComponent(text)}`)
         .then(res => res.text())
         .then(data => {
-        console.log(data)
+        console.log("URL received: " + data)
         chrome.tabs.update({'url': data.slice(1,-2)})       //weird slicing necessary to get valid url for some reason (remove "")
         })
     }) 
@@ -22,8 +23,8 @@ function newKey(message){
     chrome.identity.getProfileUserInfo()
     .then(res => res.id)
     .then(id => {
-        console.log(id)
-        fetch("http://127.0.0.1:8080/input-suggestion", {                                 //"https://searchbar-iaklo4m3da-uc.a.run.app/input-suggestion",{
+        console.log("posting new kword: " + message + "for user: " + id)
+        fetch(domain + "input-suggestion", {                                 //"https://searchbar-iaklo4m3da-uc.a.run.app/input-suggestion",{
             method: "POST",
             body: JSON.stringify({'id': id, 'new':message})
         })
@@ -32,10 +33,20 @@ function newKey(message){
 
 
 chrome.runtime.onMessage.addListener(function(message){
-    console.log("message recieved")
-    console.log(JSON.stringify(message))
-    if (message.type = 'newKey') {
+    console.log("message recieved: " + JSON.stringify(message))
+    if (message.type == "newkey") {
         newKey(message.body)
-    } else if (message.type = 'getKeys') {}
+    } else if (message.type == 'getKeys') {
+        getkeys()
+    }
     
 }) 
+
+
+function getkeys(){
+    chrome.identity.getProfileUserInfo()
+    .then(res => res.id)
+    .then(id => {
+
+    })
+}
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,7 +1,7 @@
 //document.addEventListener('DOMContentLoaded', documentEvents  , false);
-console.log("something wnflsadnf.,mdnsa.,")
+console.log("popup.js invoked")
 function submit(input1, input2) { 
-    console.log(input1.value + "   " + input2.value);
+    console.log("message sent: "+ input1.value + ":" + input2.value);
     chrome.runtime.sendMessage(message = {'type': 'newkey', 'body': {[input1.value] : input2.value}})
 }
 
@@ -13,11 +13,11 @@ const isValidUrl = urlString=> {
     return false; 
   }
 }
- console.log("jksdfjlf")  
+  
 document.getElementById('form').addEventListener('submit', 
   function() {
     
-    // console.log("something else") 
+    console.log("event triggered") 
     // var key = document.getElementById('key')
     // var url = document.getElementById('url')
 
