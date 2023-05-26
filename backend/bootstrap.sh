9a511c1fd281897086999a95382567a75aa13ae6
Julius Vidal <jgtvidal@gmail.com>
2023-05-26 21:13:25 +0100

    setup aws in docker

--- backend/bootstrap.sh
+++ backend/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 export FLASK_APP=./searchbar/main.py
+export AWS_DEFAULT_REGION=eu-west-2
+export AWS_DEFAULT_OUTPUT=json
+export AWS_ACCESS_KEY_ID=$id 
+export AWS_SECRET_ACCESS_KEY=$secret_key
 pipenv run flask --debug run -h 0.0.0.0 -p 8080
 
 # if file not found check line endings are in LF and not CRLF
